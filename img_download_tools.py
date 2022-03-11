ccd5b15e00e75e2d5a1a7cc19f289d14cc0f0ed8
DimitryRukhadze <lokiinflames@gmail.com>
2022-03-11 13:52:27 +0300

    Changed arg name for download_image()

--- img_download_tools.py
+++ img_download_tools.py
@@ -1,9 +1,9 @@
 import requests
 
 
-def download_image(img_url, save_to):
+def download_image(img_url, img_path):
     response = requests.get(img_url)
     response.raise_for_status()
 
-    with open(save_to, 'wb') as file:
+    with open(img_path, 'wb') as file:
         file.write(response.content)
